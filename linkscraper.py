10e725f0a970e6f2af4e3c89187434de5a51fc34
jenniferli1098 <jenniferli1098@gmail.com>
2020-03-12 11:05:22 -0400

    extracted links

--- linkscraper.py
+++ linkscraper.py
@@ -1,4 +1,5 @@
 import re
+import csv
 
 regex = r"<a href=\"(.*?)\"[^>]*>(.*?)-(.*?) \((.*?)\)[^>]*<"
 
@@ -8,16 +9,28 @@ with open('links.txt', 'r') as file:
 
 matches = re.finditer(regex, test_str, re.MULTILINE)
 
+
+#create an array
+arr = []
+arr.append(["Link", "Course Code", "Course Name", "Professor"])
+
 for matchNum, match in enumerate(matches, start=1):
     
     #print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-    
+    row = []
+
     for groupNum in range(0, len(match.groups())):
         groupNum = groupNum + 1
         print ("Group {groupNum} : {group}".format(groupNum = groupNum, group = match.group(groupNum)))
+        row.append(match.group(groupNum))
         #print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+    row[0] = row[0].replace("&amp;","&")
+    arr.append(row)
 
-
+with open('courses.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for row in arr:
+        writer.writerow(row)
 
 #########################
 
